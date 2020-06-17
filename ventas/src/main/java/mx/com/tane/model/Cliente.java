b2af0d1886626167e17af80ce15a4d4da38ec7f2
administrador@TANE.MX <administrador@TANE.MX>
2020-06-17 17:58:15 -0600

    Primer commit

---
+++ ventas/src/main/java/mx/com/tane/model/Cliente.java
@@ -0,0 +1,677 @@
+package mx.com.tane.model;
+
+public class Cliente {
+	private int id;
+	private String clave;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String nombre;
+	private String apellidoCasada;
+	private String calle;
+	private String numeroIntDepto;
+	private String numeroExt;
+	private String cveColonia;
+	private String cveDelegacion;
+	private String cveEstado;
+	private String cvePais;
+	private String telefono1;
+	private String telefono2;
+	private String movil1;
+	private String movil2;
+	private String telefonoTrabajo1;
+	private String telefonoTrabajo2;
+	private String cveTipoCliente;
+	private double descuento;
+	private String rfc;
+	private String numeroPiso;
+	private String nacionalidad;
+	private String cveTituloCliente;
+	private String sexo;
+	private String mail;
+	private String recibeCatalogo;
+	private String conTituloySra;
+	private String cveTienda;
+	private String fechaAlta;
+	private String razonSocial;
+	private String arteObj;
+	private String cveImpuesto;
+	private String extension1;
+	private String extension2;
+	private String cveTipodeCalle;
+	private String numeroExtra;
+	private String claveAnterior;
+	private String contacto1NombreCompleto;
+	private String contacto1Puesto;
+	private String contacto1Telefono;
+	private String contacto1Extension;
+	private String contacto1RecibeCorrespondencia;
+	private String contacto2NombreCompleto;
+	private String contacto2Puesto;
+	private String contacto2Telefono;
+	private String contacto2Extension;
+	private String contacto2RecibeCorrespondencia;
+	private String contacto3NombreCompleto;
+	private String contacto3Puesto;
+	private String contacto3Telefono;
+	private String contacto3Extension;
+	private String contacto3RecibeCorrespondencia;
+	private String contacto4NombreCompleto;
+	private String contacto4Puesto;
+	private String contacto4Telefono;
+	private String contacto4Extension;
+	private String contacto4RecibeCorrespondencia;
+	private String fechaActualizacion;
+	private String transacNombre;
+	private String transacDirCalleNums;
+	private String transacDirColCp;
+	private String transacDirDelmpioEdoPais;
+	private String transacTelefono;
+	private String editarEnTransac;
+	private String cveTiendaActualizo;
+	private String actDirTransac;
+	private String fechaReinicioAcumuladoVenta;
+	private int cantidadTransaccionesAcumuladoVenta;
+	private double acumuladoVenta;
+	private String editarEnMnto;
+	private String codigoPostal;
+	private String colonia;
+	private String delegacion;
+	private String estado;
+	private String fechaAniversario;
+	private String lldIdentificado;
+	private String lldFechaIdentificado;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellidoCasada() {
+		return apellidoCasada;
+	}
+	public void setApellidoCasada(String apellidoCasada) {
+		this.apellidoCasada = apellidoCasada;
+	}
+	
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	
+	public String getNumeroIntDepto() {
+		return numeroIntDepto;
+	}
+	public void setNumeroIntDepto(String numeroIntDepto) {
+		this.numeroIntDepto = numeroIntDepto;
+	}
+	
+	public String getNumeroExt() {
+		return numeroExt;
+	}
+	public void setNumeroExt(String numeroExt) {
+		this.numeroExt = numeroExt;
+	}
+	
+	public String getCveColonia() {
+		return cveColonia;
+	}
+	public void setCveColonia(String cveColonia) {
+		this.cveColonia = cveColonia;
+	}
+	
+	public String getCveDelegacion() {
+		return cveDelegacion;
+	}
+	public void setCveDelegacion(String cveDelegacion) {
+		this.cveDelegacion = cveDelegacion;
+	}
+	
+	public String getCveEstado() {
+		return cveEstado;
+	}
+	public void setCveEstado(String cveEstado) {
+		this.cveEstado = cveEstado;
+	}
+	
+	public String getCvePais() {
+		return cvePais;
+	}
+	public void setCvePais(String cvePais) {
+		this.cvePais = cvePais;
+	}
+	
+	public String getTelefono1() {
+		return telefono1;
+	}
+	public void setTelefono1(String telefono1) {
+		this.telefono1 = telefono1;
+	}
+	
+	public String getTelefono2() {
+		return telefono2;
+	}
+	public void setTelefono2(String telefono2) {
+		this.telefono2 = telefono2;
+	}
+	
+	public String getMovil1() {
+		return movil1;
+	}
+	public void setMovil1(String movil1) {
+		this.movil1 = movil1;
+	}
+	
+	public String getMovil2() {
+		return movil2;
+	}
+	public void setMovil2(String movil2) {
+		this.movil2 = movil2;
+	}
+	
+	public String getTelefonoTrabajo1() {
+		return telefonoTrabajo1;
+	}
+	public void setTelefonoTrabajo1(String telefonoTrabajo1) {
+		this.telefonoTrabajo1 = telefonoTrabajo1;
+	}
+	
+	public String getTelefonoTrabajo2() {
+		return telefonoTrabajo2;
+	}
+	public void setTelefonoTrabajo2(String telefonoTrabajo2) {
+		this.telefonoTrabajo2 = telefonoTrabajo2;
+	}
+	
+	public String getCveTipoCliente() {
+		return cveTipoCliente;
+	}
+	public void setCveTipoCliente(String cveTipoCliente) {
+		this.cveTipoCliente = cveTipoCliente;
+	}
+	
+	public double getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+	
+	public String getRfc() {
+		return rfc;
+	}
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+	
+	public String getNumeroPiso() {
+		return numeroPiso;
+	}
+	public void setNumeroPiso(String numeroPiso) {
+		this.numeroPiso = numeroPiso;
+	}
+	
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	
+	public String getCveTituloCliente() {
+		return cveTituloCliente;
+	}
+	public void setCveTituloCliente(String cveTituloCliente) {
+		this.cveTituloCliente = cveTituloCliente;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public String getRecibeCatalogo() {
+		return recibeCatalogo;
+	}
+	public void setRecibeCatalogo(String recibeCatalogo) {
+		this.recibeCatalogo = recibeCatalogo;
+	}
+	
+	public String getConTituloySra() {
+		return conTituloySra;
+	}
+	public void setConTituloySra(String conTituloySra) {
+		this.conTituloySra = conTituloySra;
+	}
+	
+	public String getCveTienda() {
+		return cveTienda;
+	}
+	public void setCveTienda(String cveTienda) {
+		this.cveTienda = cveTienda;
+	}
+	
+	public String getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(String fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+	
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	
+	public String getArteObj() {
+		return arteObj;
+	}
+	public void setArteObj(String arteObj) {
+		this.arteObj = arteObj;
+	}
+	
+	public String getCveImpuesto() {
+		return cveImpuesto;
+	}
+	public void setCveImpuesto(String cveImpuesto) {
+		this.cveImpuesto = cveImpuesto;
+	}
+	
+	public String getExtension1() {
+		return extension1;
+	}
+	public void setExtension1(String extension1) {
+		this.extension1 = extension1;
+	}
+	
+	public String getExtension2() {
+		return extension2;
+	}
+	public void setExtension2(String extension2) {
+		this.extension2 = extension2;
+	}
+	
+	public String getCveTipodeCalle() {
+		return cveTipodeCalle;
+	}
+	public void setCveTipodeCalle(String cveTipodeCalle) {
+		this.cveTipodeCalle = cveTipodeCalle;
+	}
+	
+	public String getNumeroExtra() {
+		return numeroExtra;
+	}
+	public void setNumeroExtra(String numeroExtra) {
+		this.numeroExtra = numeroExtra;
+	}
+	
+	public String getClaveAnterior() {
+		return claveAnterior;
+	}
+	public void setClaveAnterior(String claveAnterior) {
+		this.claveAnterior = claveAnterior;
+	}
+	
+	public String getContacto1NombreCompleto() {
+		return contacto1NombreCompleto;
+	}
+	public void setContacto1NombreCompleto(String contacto1NombreCompleto) {
+		this.contacto1NombreCompleto = contacto1NombreCompleto;
+	}
+	
+	public String getContacto1Puesto() {
+		return contacto1Puesto;
+	}
+	public void setContacto1Puesto(String contacto1Puesto) {
+		this.contacto1Puesto = contacto1Puesto;
+	}
+	
+	public String getContacto1Telefono() {
+		return contacto1Telefono;
+	}
+	public void setContacto1Telefono(String contacto1Telefono) {
+		this.contacto1Telefono = contacto1Telefono;
+	}
+	
+	public String getContacto1Extension() {
+		return contacto1Extension;
+	}
+	public void setContacto1Extension(String contacto1Extension) {
+		this.contacto1Extension = contacto1Extension;
+	}
+	
+	public String getContacto1RecibeCorrespondencia() {
+		return contacto1RecibeCorrespondencia;
+	}
+	public void setContacto1RecibeCorrespondencia(String contacto1RecibeCorrespondencia) {
+		this.contacto1RecibeCorrespondencia = contacto1RecibeCorrespondencia;
+	}
+	
+	public String getContacto2NombreCompleto() {
+		return contacto2NombreCompleto;
+	}
+	public void setContacto2NombreCompleto(String contacto2NombreCompleto) {
+		this.contacto2NombreCompleto = contacto2NombreCompleto;
+	}
+	
+	public String getContacto2Puesto() {
+		return contacto2Puesto;
+	}
+	public void setContacto2Puesto(String contacto2Puesto) {
+		this.contacto2Puesto = contacto2Puesto;
+	}
+	
+	public String getContacto2Telefono() {
+		return contacto2Telefono;
+	}
+	public void setContacto2Telefono(String contacto2Telefono) {
+		this.contacto2Telefono = contacto2Telefono;
+	}
+	
+	public String getContacto2Extension() {
+		return contacto2Extension;
+	}
+	public void setContacto2Extension(String contacto2Extension) {
+		this.contacto2Extension = contacto2Extension;
+	}
+	
+	public String getContacto2RecibeCorrespondencia() {
+		return contacto2RecibeCorrespondencia;
+	}
+	public void setContacto2RecibeCorrespondencia(String contacto2RecibeCorrespondencia) {
+		this.contacto2RecibeCorrespondencia = contacto2RecibeCorrespondencia;
+	}
+	
+	public String getContacto3NombreCompleto() {
+		return contacto3NombreCompleto;
+	}
+	public void setContacto3NombreCompleto(String contacto3NombreCompleto) {
+		this.contacto3NombreCompleto = contacto3NombreCompleto;
+	}
+	
+	public String getContacto3Puesto() {
+		return contacto3Puesto;
+	}
+	public void setContacto3Puesto(String contacto3Puesto) {
+		this.contacto3Puesto = contacto3Puesto;
+	}
+	
+	public String getContacto3Telefono() {
+		return contacto3Telefono;
+	}
+	public void setContacto3Telefono(String contacto3Telefono) {
+		this.contacto3Telefono = contacto3Telefono;
+	}
+	
+	public String getContacto3Extension() {
+		return contacto3Extension;
+	}
+	public void setContacto3Extension(String contacto3Extension) {
+		this.contacto3Extension = contacto3Extension;
+	}
+	
+	public String getContacto3RecibeCorrespondencia() {
+		return contacto3RecibeCorrespondencia;
+	}
+	public void setContacto3RecibeCorrespondencia(String contacto3RecibeCorrespondencia) {
+		this.contacto3RecibeCorrespondencia = contacto3RecibeCorrespondencia;
+	}
+	
+	public String getContacto4NombreCompleto() {
+		return contacto4NombreCompleto;
+	}
+	public void setContacto4NombreCompleto(String contacto4NombreCompleto) {
+		this.contacto4NombreCompleto = contacto4NombreCompleto;
+	}
+	
+	public String getContacto4Puesto() {
+		return contacto4Puesto;
+	}
+	public void setContacto4Puesto(String contacto4Puesto) {
+		this.contacto4Puesto = contacto4Puesto;
+	}
+	
+	public String getContacto4Telefono() {
+		return contacto4Telefono;
+	}
+	public void setContacto4Telefono(String contacto4Telefono) {
+		this.contacto4Telefono = contacto4Telefono;
+	}
+	
+	public String getContacto4Extension() {
+		return contacto4Extension;
+	}
+	public void setContacto4Extension(String contacto4Extension) {
+		this.contacto4Extension = contacto4Extension;
+	}
+	
+	public String getContacto4RecibeCorrespondencia() {
+		return contacto4RecibeCorrespondencia;
+	}
+	public void setContacto4RecibeCorrespondencia(String contacto4RecibeCorrespondencia) {
+		this.contacto4RecibeCorrespondencia = contacto4RecibeCorrespondencia;
+	}
+	
+	public String getFechaActualizacion() {
+		return fechaActualizacion;
+	}
+	public void setFechaActualizacion(String fechaActualizacion) {
+		this.fechaActualizacion = fechaActualizacion;
+	}
+	
+	public String getTransacNombre() {
+		return transacNombre;
+	}
+	public void setTransacNombre(String transacNombre) {
+		this.transacNombre = transacNombre;
+	}
+	
+	public String getTransacDirCalleNums() {
+		return transacDirCalleNums;
+	}
+	public void setTransacDirCalleNums(String transacDirCalleNums) {
+		this.transacDirCalleNums = transacDirCalleNums;
+	}
+	
+	public String getTransacDirColCp() {
+		return transacDirColCp;
+	}
+	public void setTransacDirColCp(String transacDirColCp) {
+		this.transacDirColCp = transacDirColCp;
+	}
+	
+	public String getTransacDirDelmpioEdoPais() {
+		return transacDirDelmpioEdoPais;
+	}
+	public void setTransacDirDelmpioEdoPais(String transacDirDelmpioEdoPais) {
+		this.transacDirDelmpioEdoPais = transacDirDelmpioEdoPais;
+	}
+	
+	public String getTransacTelefono() {
+		return transacTelefono;
+	}
+	public void setTransacTelefono(String transacTelefono) {
+		this.transacTelefono = transacTelefono;
+	}
+	
+	public String getEditarEnTransac() {
+		return editarEnTransac;
+	}
+	public void setEditarEnTransac(String editarEnTransac) {
+		this.editarEnTransac = editarEnTransac;
+	}
+	
+	public String getCveTiendaActualizo() {
+		return cveTiendaActualizo;
+	}
+	public void setCveTiendaActualizo(String cveTiendaActualizo) {
+		this.cveTiendaActualizo = cveTiendaActualizo;
+	}
+	
+	public String getActDirTransac() {
+		return actDirTransac;
+	}
+	public void setActDirTransac(String actDirTransac) {
+		this.actDirTransac = actDirTransac;
+	}
+	
+	public String getFechaReinicioAcumuladoVenta() {
+		return fechaReinicioAcumuladoVenta;
+	}
+	public void setFechaReinicioAcumuladoVenta(String fechaReinicioAcumuladoVenta) {
+		this.fechaReinicioAcumuladoVenta = fechaReinicioAcumuladoVenta;
+	}
+	
+	public int getCantidadTransaccionesAcumuladoVenta() {
+		return cantidadTransaccionesAcumuladoVenta;
+	}
+	public void setCantidadTransaccionesAcumuladoVenta(int cantidadTransaccionesAcumuladoVenta) {
+		this.cantidadTransaccionesAcumuladoVenta = cantidadTransaccionesAcumuladoVenta;
+	}
+	
+	public double getAcumuladoVenta() {
+		return acumuladoVenta;
+	}
+	public void setAcumuladoVenta(double acumuladoVenta) {
+		this.acumuladoVenta = acumuladoVenta;
+	}
+	
+	public String getEditarEnMnto() {
+		return editarEnMnto;
+	}
+	public void setEditarEnMnto(String editarEnMnto) {
+		this.editarEnMnto = editarEnMnto;
+	}
+	
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	
+	public String getColonia() {
+		return colonia;
+	}
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+	
+	public String getDelegacion() {
+		return delegacion;
+	}
+	public void setDelegacion(String delegacion) {
+		this.delegacion = delegacion;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public String getFechaAniversario() {
+		return fechaAniversario;
+	}
+	public void setFechaAniversario(String fechaAniversario) {
+		this.fechaAniversario = fechaAniversario;
+	}
+	
+	public String getLldIdentificado() {
+		return lldIdentificado;
+	}
+	public void setLldIdentificado(String lldIdentificado) {
+		this.lldIdentificado = lldIdentificado;
+	}
+	
+	public String getLldFechaIdentificado() {
+		return lldFechaIdentificado;
+	}
+	public void setLldFechaIdentificado(String lldFechaIdentificado) {
+		this.lldFechaIdentificado = lldFechaIdentificado;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", clave=" + clave + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno="
+				+ apellidoMaterno + ", nombre=" + nombre + ", apellidoCasada=" + apellidoCasada + ", calle=" + calle
+				+ ", numeroIntDepto=" + numeroIntDepto + ", numeroExt=" + numeroExt + ", cveColonia=" + cveColonia
+				+ ", cveDelegacion=" + cveDelegacion + ", cveEstado=" + cveEstado + ", cvePais=" + cvePais
+				+ ", telefono1=" + telefono1 + ", telefono2=" + telefono2 + ", movil1=" + movil1 + ", movil2=" + movil2
+				+ ", telefonoTrabajo1=" + telefonoTrabajo1 + ", telefonoTrabajo2=" + telefonoTrabajo2
+				+ ", cveTipoCliente=" + cveTipoCliente + ", descuento=" + descuento + ", rfc=" + rfc + ", numeroPiso="
+				+ numeroPiso + ", nacionalidad=" + nacionalidad + ", cveTituloCliente=" + cveTituloCliente + ", sexo="
+				+ sexo + ", mail=" + mail + ", recibeCatalogo=" + recibeCatalogo + ", conTituloySra=" + conTituloySra
+				+ ", cveTienda=" + cveTienda + ", fechaAlta=" + fechaAlta + ", razonSocial=" + razonSocial
+				+ ", arteObj=" + arteObj + ", cveImpuesto=" + cveImpuesto + ", extension1=" + extension1
+				+ ", extension2=" + extension2 + ", cveTipodeCalle=" + cveTipodeCalle + ", numeroExtra=" + numeroExtra
+				+ ", claveAnterior=" + claveAnterior + ", contacto1NombreCompleto=" + contacto1NombreCompleto
+				+ ", contacto1Puesto=" + contacto1Puesto + ", contacto1Telefono=" + contacto1Telefono
+				+ ", contacto1Extension=" + contacto1Extension + ", contacto1RecibeCorrespondencia="
+				+ contacto1RecibeCorrespondencia + ", contacto2NombreCompleto=" + contacto2NombreCompleto
+				+ ", contacto2Puesto=" + contacto2Puesto + ", contacto2Telefono=" + contacto2Telefono
+				+ ", contacto2Extension=" + contacto2Extension + ", contacto2RecibeCorrespondencia="
+				+ contacto2RecibeCorrespondencia + ", contacto3NombreCompleto=" + contacto3NombreCompleto
+				+ ", contacto3Puesto=" + contacto3Puesto + ", contacto3Telefono=" + contacto3Telefono
+				+ ", contacto3Extension=" + contacto3Extension + ", contacto3RecibeCorrespondencia="
+				+ contacto3RecibeCorrespondencia + ", contacto4NombreCompleto=" + contacto4NombreCompleto
+				+ ", contacto4Puesto=" + contacto4Puesto + ", contacto4Telefono=" + contacto4Telefono
+				+ ", contacto4Extension=" + contacto4Extension + ", contacto4RecibeCorrespondencia="
+				+ contacto4RecibeCorrespondencia + ", fechaActualizacion=" + fechaActualizacion + ", transacNombre="
+				+ transacNombre + ", transacDirCalleNums=" + transacDirCalleNums + ", transacDirColCp="
+				+ transacDirColCp + ", transacDirDelmpioEdoPais=" + transacDirDelmpioEdoPais + ", transacTelefono="
+				+ transacTelefono + ", editarEnTransac=" + editarEnTransac + ", cveTiendaActualizo="
+				+ cveTiendaActualizo + ", actDirTransac=" + actDirTransac + ", fechaReinicioAcumuladoVenta="
+				+ fechaReinicioAcumuladoVenta + ", cantidadTransaccionesAcumuladoVenta="
+				+ cantidadTransaccionesAcumuladoVenta + ", acumuladoVenta=" + acumuladoVenta + ", editarEnMnto="
+				+ editarEnMnto + ", codigoPostal=" + codigoPostal + ", colonia=" + colonia + ", delegacion="
+				+ delegacion + ", estado=" + estado + ", fechaAniversario=" + fechaAniversario + ", lldIdentificado="
+				+ lldIdentificado + ", lldFechaIdentificado=" + lldFechaIdentificado + "]";
+	}
+	
+	
+
+}
